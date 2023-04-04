5574f8ed3406334040ba349018e2cee63e2c4bb0
vickneee <victoria.vavulina@outlook.com>
2023-04-05 01:12:56 +0300

    JS file ch

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-function message() {
 
-    let name = document.getElementById("name").value;
-    let age = Number(document.getElementById("age").value);
+let name = document.getElementById("name").value;
+let age = Number(document.getElementById("age").value);
 
-    if (age >= 18) {
-        document.getElementById("message").innerHTML = `Hello ${name}, you are an adult.`
-    } else if (age < 18) {
-        document.getElementById("message").innerHTML = `Hello ${name}, you are not yet an adult.`
-    }
+if (age >= 18) {
+    document.getElementById("message").innerHTML = `Hello ${name}, you are an adult.`
+} else if (age < 18) {
+    document.getElementById("message").innerHTML = `Hello ${name}, you are not yet an adult.`
 }
