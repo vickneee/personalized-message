45099a679217e6f073d1d993f7448ac9a78837dd
vickneee <victoria.vavulina@outlook.com>
2023-04-05 14:15:41 +0300

    Script, HTML corr

--- script.js
+++
@@ -1,9 +0,0 @@
-
-let name = document.getElementById("name").value;
-let age = Number(document.getElementById("age").value);
-
-if (age >= 18) {
-    document.getElementById("message").innerHTML = `Hello ${name}, you are an adult.`
-} else if (age < 18) {
-    document.getElementById("message").innerHTML = `Hello ${name}, you are not yet an adult.`
-}
